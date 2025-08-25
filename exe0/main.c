302ace28b7f1c7e59a53414fea3452e5e9e08d18
Luigi <luigics@al.insper.edu.br>
2025-08-24 21:31:08 -0300

    exe0 fix

--- exe0/main.c
+++ exe0/main.c
@@ -7,8 +7,7 @@
 #include <stdio.h>
 
 const int BTN_PIN_R = 28;
-
-int btn_flag;
+volatile int btn_flag = 0;
 
 void btn_callback(uint gpio, uint32_t events) {
   if (events == 0x4) { // fall edge
@@ -35,8 +34,8 @@ int main() {
   gpio_set_irq_enabled_with_callback(BTN_PIN_R, GPIO_IRQ_EDGE_FALL, true,
                                      &btn_callback);
 
-  volatile int capture_flag = 0;
-  int a;
+  int capture_flag = 0;
+  
   while (1) {
     if (btn_flag) {
       capture_flag = 1;
